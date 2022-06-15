edd2ea06f7c3132eb4d947cafce818a3e7cfadff
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-06-15 10:27:25 +0100

    2.12* when only one country matching the query,

--- part-2/2.12/src/App.js
+++ part-2/2.12/src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [results, setResults] = useState([]);
-  const [showWarning, setShowWarning] = useState(false);
 
   useEffect(() => {
     getData();
@@ -13,6 +12,7 @@ const App = () => {
   const getData = async (e) => {
     try {
       const { data } = await axios("https://restcountries.com/v3.1/all");
+      console.log(data);
       setCountries(data);
     } catch (error) {
       console.log(error);
@@ -20,15 +20,12 @@ const App = () => {
   };
 
   const onChangeHandler = (e) => {
-    if ((e.target.value !== "") & (countries.length > 0)) {
+    if (e.target.value !== "" && countries.length > 0) {
       let filteredResults = countries.filter((c) =>
-        c.name.common.includes(e.target.value)
+        c.name.common.toLowerCase().includes(e.target.value.toLowerCase())
       );
 
-      if (filteredResults.length > 10) {
-        setShowWarning(true);
-      } else {
-        setShowWarning(false);
+      if (filteredResults.length <= 10) {
         setResults(filteredResults);
       }
     } else {
@@ -41,10 +38,20 @@ const App = () => {
       <h1>Countries</h1>
       <input type="text" onChange={onChangeHandler} />
       <ul>
-        {results &&
-          results.map((i) => <li key={i.name.common}>{i.name.common}</li>)}
+        {results.length === 1 ? (
+          <>
+            <h2>{results[0].name.common}</h2>
+            <li>Capital: {results[0].capital}</li>
+            <li>Population: {results[0].population}</li>
+            <li>Languages: </li>
+            <img width="150px" src={results[0].flags.png} alt="flags" />
+          </>
+        ) : results.length <= 10 && results.length ? (
+          results.map((i) => <h2 key={i.name.common}>{i.name.common}</h2>)
+        ) : (
+          <p>Please fine tune your search</p>
+        )}
       </ul>
-      {showWarning && <p>Please fine tune your search</p>}
     </div>
   );
 };
