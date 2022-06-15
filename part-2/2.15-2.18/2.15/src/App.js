af89014c80daacf54125103c33df42153558a52b
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-06-15 15:20:30 +0100

    2.15: Phonebook step7

---
+++ part-2/2.15-2.18/2.15/src/App.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { Filter, PersonForm } from "./components";
+import axios from "axios";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [number, setNumber] = useState("");
+  const [search, setSearch] = useState("");
+  const [reRender, setReRender] = useState(false);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/persons");
+      setPersons(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    setReRender(false);
+  }, [reRender]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: number,
+    };
+    const isPresent = persons.filter(
+      (element) => element.name === newPerson.name
+    );
+
+    if (isPresent.length) {
+      alert(`${newPerson.name} is already added to the phonebook`);
+      setNewName("");
+      setNumber("");
+    } else {
+      try {
+        await axios.post("http://localhost:3001/persons", newPerson);
+        setNewName("");
+        setNumber("");
+        setReRender(true);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    setNewName("");
+    setNumber("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter setSearch={setSearch} />
+      <PersonForm
+        newName={newName}
+        number={number}
+        setNumber={setNumber}
+        handleSubmit={handleSubmit}
+        setNewName={setNewName}
+      />
+
+      <h2>Numbers</h2>
+      {persons
+        .filter((person) => {
+          return person.name.toLowerCase().includes(search.toLowerCase());
+        })
+        .map((person, index) => {
+          return (
+            <h3 key={index + 1}>
+              {person.name} {person.number}
+            </h3>
+          );
+        })}
+    </div>
+  );
+};
+
+export default App;
